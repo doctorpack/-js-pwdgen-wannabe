b883fab1a66b9111664a7eafee2e8c5a3d4b33eb
doctorpack <enricopachera@gmail.com>
2022-10-20 15:56:57 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let richiestanome =prompt("qual'è il tuo nome?");
 let richiestacognome = prompt("qual'è il tuo cognome?");
 let richiestacolore = prompt("qualè il tuo colore preferito?");
-document.getElementById("rispostanome").innerHTML = richiestanome;
-document.getElementById("rispostacognome").innerHTML = richiestacognome;
-document.getElementById("rispostacolore").innerHTML = richiestacolore;
-document.getElementById ('somma').innerHTML = 'rispostanomerispostacognomerichiestacolore22';
+document.getElementById("richiestanome").innerHTML = richiestanome;
+document.getElementById("richiestacognome").innerHTML = richiestacognome;
+document.getElementById("richiestacolore").innerHTML = richiestacolore;
+document.getElementById ('somma').innerHTML = 'richiestanomerichiestacognomerichiestacolore22';
